9612bc121db088158cd3cbf84886156d62ad414b
EagleSixty6 <daniel.zielasko@rwth-aachen.de>
2021-04-24 00:00:21 +0200

    initial commit

---
+++ Assets/scripts/animation/DynmicWaterLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynmicWaterLevel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0,Mathf.Sin(Time.time) + 1,0); 
+    }
+}
